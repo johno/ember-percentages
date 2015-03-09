e66ae019b8076a7e04580fce3dca4e169daa9adf
John Otander <johnotander@gmail.com>
2015-03-08 21:49:14 -0600

    Upgrade CLI, bump to 1.0.1

--- tests/unit/helpers/percentage-test.js
+++ tests/unit/helpers/percentage-test.js
@@ -1,25 +1,26 @@
 import {
   percentage
 } from 'ember-percentages/helpers/percentage';
+import { module, test } from 'qunit';
 
 module('PercentageHelper');
 
-test('it returns a percentage without a trailing .0', function() {
-  equal(percentage(0.42), '42%');
+test('it returns a percentage without a trailing .0', function(assert) {
+  assert.equal(percentage(0.42), '42%');
 });
 
-test('it correctly rounds the percentage up where appropriate', function() {
-  equal(percentage(0.6789, 1), '67.9%');
+test('it correctly rounds the percentage up where appropriate', function(assert) {
+  assert.equal(percentage(0.6789, 1), '67.9%');
 });
 
-test('it correctly rounds the percentage down where appropriate', function() {
-  equal(percentage(0.6788881, 4), '67.8888%');
+test('it correctly rounds the percentage down where appropriate', function(assert) {
+  assert.equal(percentage(0.6788881, 4), '67.8888%');
 });
 
-test('it correctly handles 1', function() {
-  equal(percentage(1), '100%');
+test('it correctly handles 1', function(assert) {
+  assert.equal(percentage(1), '100%');
 });
 
-test('it correctly handles 0', function() {
-  equal(percentage(0), '0%');
+test('it correctly handles 0', function(assert) {
+  assert.equal(percentage(0), '0%');
 });
